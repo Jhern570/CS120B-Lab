1a98afbb92299c6473894dd88c081c72df1cc9ab
Jhern570 <juliohdezjrn@gmail.com>
2021-10-23 23:09:36 -0700

    Lab7

---
+++ jhern570_lab7_part1.c
@@ -0,0 +1,156 @@
+/*	Author: Julio Hernandez 
+ *	Lab Section: 023
+ *	Assignment: Lab #7  Exercise # 1
+ * 
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "../header/simAVRHeader.h"
+#include "../header/timer.h"
+#include "../header/io.h"
+#endif
+
+
+enum States{start, wait, press0, press1, pressBoth}state;
+unsigned char cnt = 0;
+unsigned char button;
+unsigned char i;
+void Tick(){
+	button = ~(PINA);
+	switch(state){
+		case start: 
+			LCD_WriteData(cnt + '0');
+			state = wait;
+		case wait:
+			if(button == 1){
+				if(cnt != 9){
+					cnt++;
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+				i = 0;
+				state = press0;
+			}
+			else if(button == 2){
+				if(cnt != 0){
+					cnt--;
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+				i = 0;
+				state = press1;
+			}
+			else if(button == 3){
+				cnt = 0;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				state = pressBoth;
+			}
+			else if(button == 0){
+				state = wait;
+			}
+			break;
+		case pressBoth:
+			if(button == 3){
+				state = pressBoth;
+			}
+			else if(button == 1){
+				cnt++;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				i = 0;
+				state = press0;
+			}
+			else if(button == 2){
+				i = 0;
+				state = press1;
+			}
+			else if(button == 0){
+				state = wait;
+			}
+			break;
+		case press0:
+			i++;
+			if(button == 1){
+				if(cnt != 9 && i > 10){
+					i = 0;
+					cnt++;
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+				state = press0;
+			}
+			else if(button == 2){
+				cnt--;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				i = 0;
+				state = press1;
+			}
+			else if(button == 3 ){
+				cnt = 0;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				state = pressBoth;
+			}
+			else if(button == 0){
+				state = wait;
+			}
+			break;
+		case press1:
+			i++;
+			if(button == 2){
+				if(cnt != 0 && i > 10){
+					i = 0;
+					cnt--;
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+				state = press1;
+			}
+			else if(button == 1){
+				i = 0;
+				cnt++;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				state = press0;
+			}
+			else if(button == 3){
+				cnt = 0;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+				state = pressBoth;
+			}
+			else if(button == 0){
+				state = wait;
+			}
+			break;
+		default:
+			state = start;
+			break;
+	}
+}
+					
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00; 
+    DDRD = 0xFF; PORTD = 0x00;
+    TimerSet(100);
+    TimerOn();
+    state = start;
+    //Initialize the LCD display
+    LCD_init();
+
+   
+    while (1) {
+	Tick();
+	while(!TimerFlag);
+	TimerFlag = 0;
+    }
+
+    return 1;
+}
+
---
+++ jhern570_lab7_part2.c
@@ -0,0 +1,135 @@
+/*	Author: Julio Hernandez 
+ *	Lab Section: 023
+ *	Assignment: Lab #7  Exercise # 2
+ * 
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "../header/simAVRHeader.h"
+#include "../header/timer.h"
+#include "../header/io.h"
+#endif
+
+enum States{start, on1, on2, on3, press, release}state;
+unsigned char tmpB = 0x00;
+unsigned char cnt = 0;
+unsigned char button;
+unsigned char i;
+void Tick(){
+        button = ~(PINA);
+        switch(state){
+                case start:
+                        PORTB = 0x01;
+                        i = 0;
+			LCD_WriteData(cnt + '0');
+                        state = on1;
+                        break;
+                case on1:
+                        PORTB = 0x01;
+                        i = 1;
+                        if(button == 1){
+				if(cnt != 0 ){
+					cnt--;
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+                                state = press;
+                        }
+                        else{
+                                state = on2;
+                                i++;
+                        }
+                        break;
+                case press:
+                        if(button == 1){
+                                state = press;
+                        }
+                        else{
+                                state = release;
+                        }
+                        break;
+		case release:
+                        if(button == 0){
+                                state = release;
+                        }
+                        else if(button == 1 && i == 1){
+                                state = on1;
+                        }
+                        else if(button == 1 && (i == 2 || i == 4)){
+                                if(cnt == 10){
+                                        cnt = 0;
+                                        LCD_ClearScreen();
+                                        LCD_WriteData(cnt + '0');
+                                }
+
+				state = on2;
+                        }
+                        else if(button == 1 && i == 3){
+                                state = on3;
+                        }
+                        break;
+                case on2:
+                        PORTB = 0X02;
+                        if(button == 1){
+				cnt++;
+				if(cnt < 10){
+					LCD_ClearScreen();
+					LCD_WriteData(cnt + '0');
+				}
+				else if(cnt == 10){
+					LCD_ClearScreen();
+					LCD_DisplayString(1, "YOU\'VE WON!!! XD");
+				}	
+                                state = press;
+                        }
+                        else if(button == 0 && i == 2) {
+                                state = on3;
+                                i++;
+                        }
+                        else if(button == 0 && i == 4){
+                                state = on1;
+                        }
+                        break;
+                case on3:
+                        PORTB = 0x04;
+                        if(button == 1){
+				if(cnt != 0 ){
+                                        cnt--;
+                                        LCD_ClearScreen();
+                                        LCD_WriteData(cnt + '0');
+                                }
+                                state = press;
+                        }
+                        else{
+                                state = on2;
+                                i++;
+                        }
+                        break;
+                default:
+                        state = start;
+                        break;
+        }
+}
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00; 
+    DDRD = 0xFF; PORTD = 0x00;
+    TimerSet(300);
+    TimerOn();
+    state = start;
+    //Initialize the LCD display
+    LCD_init();
+
+    while (1) {
+	Tick();
+	while(!TimerFlag);
+	TimerFlag = 0;
+    }
+
+    return 1;
+}
+
